924cbd673a94f37058180011b6a139b48cc14c7f
Nicolas Werner <n.werner@famedly.com>
2024-01-09 22:32:28 +0100

    chore: move classes to separate files

---
+++ synapse_invite_checker/handlers.py
@@ -0,0 +1,116 @@
+# Copyright (C) 2020,2023 Famedly
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+import re
+from http import HTTPStatus
+
+from pydantic import ValidationError
+from synapse.http.server import JsonResource
+from synapse.http.servlet import RestServlet, parse_and_validate_json_object_from_request
+from synapse.http.site import SynapseRequest
+from synapse.module_api import errors
+from synapse.types import JsonDict
+
+import synapse_invite_checker.invite_checker
+from synapse_invite_checker.config import InviteCheckerConfig
+from synapse_invite_checker.types import Contact
+
+
+def invite_checker_pattern(path_regex: str, config: InviteCheckerConfig):
+    path = path_regex.removeprefix("/")
+    root = config.api_prefix.removesuffix("/")
+    raw_regex = f"^{root}/{path}"
+
+    # we need to strip the /$, otherwise we can't register for the root of the prefix in a handler...
+    if raw_regex.endswith("/$"):
+        raw_regex = raw_regex.replace("/$", "$")
+
+    return [re.compile(raw_regex)]
+
+
+class InfoResource(RestServlet):
+    def __init__(self, checker: synapse_invite_checker.invite_checker.InviteChecker):
+        super().__init__()
+        self.checker = checker
+        self.PATTERNS = invite_checker_pattern("$", self.checker.config)
+
+    # @override
+    async def on_GET(self, _: SynapseRequest) -> tuple[int, JsonDict]:
+        return HTTPStatus.OK, {
+            "title": self.checker.config.title,
+            "description": self.checker.config.description,
+            "contact": self.checker.config.contact,
+            "version": self.checker.__version__,
+        }
+
+class ContactsResource(RestServlet):
+    def __init__(self, checker: synapse_invite_checker.invite_checker.InviteChecker):
+        super().__init__()
+        self.checker = checker
+        self.PATTERNS = invite_checker_pattern("/contacts$", self.checker.config)
+
+    # @override
+    async def on_GET(self, request: SynapseRequest) -> tuple[int, JsonDict]:
+        requester = await self.checker.api.get_user_by_req(request)
+        return HTTPStatus.OK, self.checker.get_contacts(requester.user).model_dump()
+
+    async def on_POST(self, request: SynapseRequest) -> tuple[int, JsonDict]:
+        return await self.on_PUT(request)
+
+    async def on_PUT(self, request: SynapseRequest) -> tuple[int, JsonDict]:
+        requester = await self.checker.api.get_user_by_req(request)
+
+        try:
+            contact = parse_and_validate_json_object_from_request(request, Contact)
+            self.checker.add_contact(requester.user, contact)
+        except (errors.SynapseError, ValidationError) as e:
+            raise errors.SynapseError(
+                    HTTPStatus.BAD_REQUEST,
+                    "Missing required field",
+                    errors.Codes.BAD_JSON,
+                    ) from e
+
+        return HTTPStatus.OK, contact.model_dump()
+
+class ContactResource(RestServlet):
+    def __init__(self, checker: synapse_invite_checker.invite_checker.InviteChecker):
+        super().__init__()
+        self.checker = checker
+        self.PATTERNS = invite_checker_pattern("/contacts/(?P<mxid>[^/]*)$", self.checker.config)
+
+    # @override
+    async def on_GET(self, request: SynapseRequest, mxid: str) -> tuple[int, JsonDict]:
+        requester = await self.checker.api.get_user_by_req(request)
+
+        contact = self.checker.get_contact(requester.user, mxid)
+        if contact:
+            return HTTPStatus.OK, contact.model_dump()
+
+        return HTTPStatus.NOT_FOUND, {}
+
+
+    async def on_DELETE(self, request: SynapseRequest, mxid: str) -> tuple[int, JsonDict]:
+        requester = await self.checker.api.get_user_by_req(request)
+
+        contact = self.checker.get_contact(requester.user, mxid)
+        if contact:
+            self.checker.del_contact(requester.user, mxid)
+            return HTTPStatus.NO_CONTENT, {}
+
+        return HTTPStatus.NOT_FOUND, {}
+
+def register_handlers(checker: synapse_invite_checker.invite_checker.InviteChecker, resource: JsonResource):
+        InfoResource(checker).register(resource)
+        ContactsResource(checker).register(resource)
+        ContactResource(checker).register(resource)
--- synapse_invite_checker/invite_checker.py
+++ synapse_invite_checker/invite_checker.py
@@ -13,54 +13,24 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 import logging
-import re
-from dataclasses import dataclass
-from http import HTTPStatus
 from collections import defaultdict
-from typing import Literal, List, Set
+from typing import Literal
 
-from pydantic import BaseModel, ConfigDict, ValidationError
 from synapse.api.constants import AccountDataTypes
 from synapse.http.server import JsonResource
-from synapse.http.servlet import RestServlet, parse_and_validate_json_object_from_request
-from synapse.http.site import SynapseRequest
 from synapse.module_api import NOT_SPAM, ModuleApi, errors
-from synapse.types import JsonDict, UserID
+from synapse.types import UserID
+
+from synapse_invite_checker.config import InviteCheckerConfig
+from synapse_invite_checker.types import Contact, Contacts
 
 logger = logging.getLogger(__name__)
-
-class InviteSettings(BaseModel):
-    model_config = ConfigDict(strict=True, frozen=True, extra="ignore", allow_inf_nan=False)
-
-    start: int
-    end: int | None = None
-
-class Contact(BaseModel):
-    model_config = ConfigDict(strict=True, frozen=True, extra="ignore", allow_inf_nan=False)
-
-    displayName: str # noqa: N815
-    mxid: str
-    inviteSettings: InviteSettings # noqa: N815
-
-class Contacts(BaseModel):
-    model_config = ConfigDict(strict=True, frozen=True, extra="ignore", allow_inf_nan=False)
-
-    contacts : list[Contact]
-
-
-@dataclass
-class InviteCheckerConfig:
-    api_prefix: str = "/_synapse/client/com.famedly/tim/v1"
-    title: str = "Invite Checker module by Famedly"
-    description: str = "Invite Checker module by Famedly"
-    contact: str = "info@famedly.com"
-
 
 class InviteChecker:
     __version__ = "0.0.1"
 
     def __init__(self, config: InviteCheckerConfig, api: ModuleApi):
-        self._contacts_by_user: dict[UserID, List[Contact]] = defaultdict(lambda: [])
+        self._contacts_by_user: dict[UserID, list[Contact]] = defaultdict(list)
         self.api = api
 
         self.config = config
@@ -68,9 +38,9 @@ class InviteChecker:
 
         self.resource = JsonResource(api._hs)
 
-        InfoResource(self).register(self.resource)
+        from synapse_invite_checker.handlers import ContactsResource, register_handlers
+        register_handlers(self, self.resource)
         ContactsResource(self).register(self.resource)
-        ContactResource(self).register(self.resource)
 
         self.api.register_web_resource(f"{config.api_prefix}", self.resource)
         logger.info("Module initialized at %s", config.api_prefix)
@@ -124,88 +94,4 @@ class InviteChecker:
             if contact.mxid == mxid:
                 return contact
         return None
-
-
-def invite_checker_pattern(path_regex: str, config: InviteCheckerConfig):
-    path = path_regex.removeprefix("/")
-    root = config.api_prefix.removesuffix("/")
-    raw_regex = f"^{root}/{path}"
-
-    # we need to strip the /$, otherwise we can't register for the root of the prefix in a handler...
-    if raw_regex.endswith("/$"):
-        raw_regex = raw_regex.replace("/$", "$")
-
-    return [re.compile(raw_regex)]
-
-
-class InfoResource(RestServlet):
-    def __init__(self, checker: InviteChecker):
-        super().__init__()
-        self.checker = checker
-        self.PATTERNS = invite_checker_pattern("$", self.checker.config)
-
-    # @override
-    async def on_GET(self, _: SynapseRequest) -> tuple[int, JsonDict]:
-        return HTTPStatus.OK, {
-            "title": self.checker.config.title,
-            "description": self.checker.config.description,
-            "contact": self.checker.config.contact,
-            "version": self.checker.__version__,
-        }
-
-class ContactsResource(RestServlet):
-    def __init__(self, checker: InviteChecker):
-        super().__init__()
-        self.checker = checker
-        self.PATTERNS = invite_checker_pattern("/contacts$", self.checker.config)
-
-    # @override
-    async def on_GET(self, request: SynapseRequest) -> tuple[int, JsonDict]:
-        requester = await self.checker.api.get_user_by_req(request)
-        return HTTPStatus.OK, self.checker.get_contacts(requester.user).model_dump()
-
-    async def on_POST(self, request: SynapseRequest) -> tuple[int, JsonDict]:
-        return await self.on_PUT(request)
-
-    async def on_PUT(self, request: SynapseRequest) -> tuple[int, JsonDict]:
-        requester = await self.checker.api.get_user_by_req(request)
-
-        try:
-            contact = parse_and_validate_json_object_from_request(request, Contact)
-            self.checker.add_contact(requester.user, contact)
-        except (errors.SynapseError, ValidationError) as e:
-            raise errors.SynapseError(
-                    HTTPStatus.BAD_REQUEST,
-                    "Missing required field",
-                    errors.Codes.BAD_JSON,
-                    ) from e
-
-        return HTTPStatus.OK, contact.model_dump()
-
-class ContactResource(RestServlet):
-    def __init__(self, checker: InviteChecker):
-        super().__init__()
-        self.checker = checker
-        self.PATTERNS = invite_checker_pattern("/contacts/(?P<mxid>[^/]*)$", self.checker.config)
-
-    # @override
-    async def on_GET(self, request: SynapseRequest, mxid: str) -> tuple[int, JsonDict]:
-        requester = await self.checker.api.get_user_by_req(request)
-
-        contact = self.checker.get_contact(requester.user, mxid)
-        if contact:
-            return HTTPStatus.OK, contact.model_dump()
-        else:
-            return HTTPStatus.NOT_FOUND, {}
-
-
-    async def on_DELETE(self, request: SynapseRequest, mxid: str) -> tuple[int, JsonDict]:
-        requester = await self.checker.api.get_user_by_req(request)
-
-        contact = self.checker.get_contact(requester.user, mxid)
-        if contact:
-            self.checker.del_contact(requester.user, mxid)
-            return HTTPStatus.NO_CONTENT, {}
-        else:
-            return HTTPStatus.NOT_FOUND, {}
 
