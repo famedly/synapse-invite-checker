e976f8c82db67df84059286d349f86e7c7a437d0
Nicolas Werner <n.werner@famedly.com>
2025-02-25 18:28:56 +0100

    chore: Bump version to 0.3.0

--- synapse_invite_checker/invite_checker.py
+++ synapse_invite_checker/invite_checker.py
@@ -171,7 +171,7 @@ BASE_API_PREFIX = "/_synapse/client/com.famedly/tim"
 
 
 class InviteChecker:
-    __version__ = "0.2.0"
+    __version__ = "0.3.0"
 
     def __init__(self, config: InviteCheckerConfig, api: ModuleApi):
         self.api = api
