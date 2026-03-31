a00b119d751bdfcc468047a870efae44670cc1ff
Jason Little <j.little@famedly.com>
2026-03-31 10:15:52 -0500

    chore: test more room versions in tests, from "9" to "12"

--- tests/test_redaction.py
+++ tests/test_redaction.py
@@ -15,6 +15,7 @@
 import logging
 from http import HTTPStatus
 
+from parameterized import parameterized_class
 from synapse.server import HomeServer
 from synapse.util.clock import Clock
 from twisted.internet.testing import MemoryReactor
@@ -46,12 +47,22 @@ def _redact_event_helper(
     return channel.json_body
 
 
+@parameterized_class(
+    ("DEFAULT_ROOM_VERSION",),
+    [
+        ("9",),
+        ("10",),
+        ("11",),
+        ("12",),
+    ],
+)
 class RedactionTimeLimitTestCase(FederatingModuleApiTestCase):
     """
     Test that redactions of events older than 24 hours are rejected in TIM version 1.2.
     """
 
     TIM_VERSION = TimVersion.V1_2
+    ALLOWED_ROOM_VERSIONS = ["9", "10", "11", "12"]
 
     def prepare(self, reactor: MemoryReactor, clock: Clock, homeserver: HomeServer):
         super().prepare(reactor, clock, homeserver)
@@ -72,7 +83,7 @@ class RedactionTimeLimitTestCase(FederatingModuleApiTestCase):
         )
 
     def test_redaction_within_24h_allowed(self) -> None:
-        room_id = self.create_local_room(self.user_a, [], False)
+        room_id = self.create_local_room(self.user_a, is_public=False)
         assert room_id is not None
 
         body = self.helper.send(
@@ -89,7 +100,7 @@ class RedactionTimeLimitTestCase(FederatingModuleApiTestCase):
         )
 
     def test_redaction_after_24h_rejected(self) -> None:
-        room_id = self.create_local_room(self.user_a, [], False)
+        room_id = self.create_local_room(self.user_a, is_public=False)
         assert room_id is not None
 
         body = self.helper.send(
@@ -109,7 +120,7 @@ class RedactionTimeLimitTestCase(FederatingModuleApiTestCase):
         )
 
     def test_redaction_after_24h_allowed_for_admin(self) -> None:
-        room_id = self.create_local_room(self.admin_user, [], False)
+        room_id = self.create_local_room(self.admin_user, is_public=False)
         assert room_id is not None
 
         body = self.helper.send(
@@ -129,12 +140,22 @@ class RedactionTimeLimitTestCase(FederatingModuleApiTestCase):
         )
 
 
+@parameterized_class(
+    ("DEFAULT_ROOM_VERSION",),
+    [
+        ("9",),
+        ("10",),
+        ("11",),
+        ("12",),
+    ],
+)
 class RedactionTimeLimitV1_1TestCase(FederatingModuleApiTestCase):
     """
     Test that redactions are NOT restricted by time in TIM version 1.1.
     """
 
     TIM_VERSION = TimVersion.V1_1
+    ALLOWED_ROOM_VERSIONS = ["9", "10", "11", "12"]
 
     def prepare(self, reactor: MemoryReactor, clock: Clock, homeserver: HomeServer):
         super().prepare(reactor, clock, homeserver)
@@ -153,7 +174,7 @@ class RedactionTimeLimitV1_1TestCase(FederatingModuleApiTestCase):
         )
 
     def test_redaction_after_24h_allowed_in_v1_1(self) -> None:
-        room_id = self.create_local_room(self.user_a, [], False)
+        room_id = self.create_local_room(self.user_a, is_public=False)
         assert room_id is not None
 
         body = self.helper.send(
--- tests/test_room_versions.py
+++ tests/test_room_versions.py
@@ -13,21 +13,73 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 from http import HTTPStatus
+from typing import Any
 
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from synapse.server import HomeServer
 from synapse.util.clock import Clock
 from twisted.internet.testing import MemoryReactor
 
-from tests.base import FederatingModuleApiTestCase, construct_extra_content
+from tests.base import FederatingModuleApiTestCase
 from tests.server import make_request
 
 
+@parameterized_class(
+    (
+        "DEFAULT_ROOM_VERSION",
+        "ALLOWED_ROOM_VERSIONS",
+        "ROOM_VERSIONS_THAT_SHOULD_FAIL",
+        "ROOM_UPGRADE_AND_DOWNGRADE_PAIRINGS",
+        "ROOM_UPGRADE_AND_DOWNGRADE_FAILURES",
+    ),
+    [
+        (
+            "9",
+            ["9", "10"],
+            ["8", 11, "bad_version"],
+            [("9", "10"), ("10", "9")],
+            ["8", "11"],
+        ),
+        (
+            "10",
+            ["10", "11"],
+            ["9", "not_real", 9],
+            [("10", "11"), ("11", "10")],
+            ["9", "12"],
+        ),
+        (
+            "11",
+            ["11", "12"],
+            ["10", "should_fail"],
+            [("11", "12"), ("12", "11")],
+            ["10"],
+        ),
+        (
+            "12",
+            ["11", "12"],
+            ["9", 1, ["bad_version"]],
+            [("11", "12"), ("12", "11")],
+            ["10"],
+        ),
+    ],
+)
 class RoomVersionCreateRoomTest(FederatingModuleApiTestCase):
     """
-    Tests for limiting room versions when creating rooms. Use the defaults of room
-    versions "9" or "10"
+    Tests for limiting room versions when upgrading and downgrading rooms.
     """
+
+    # This is defined through the parameterize_class
+    # ALLOWED_ROOM_VERSIONS
+    # One test function checks these iteratively for failure. All should BAD_REQUEST
+    ROOM_VERSIONS_THAT_SHOULD_FAIL: list[Any]
+    # Tuples of pairings of room versions used to verify upgrades and downgrades. Going
+    # either direction should work as long as both are inside the ALLOWED_ROOM_VERSIONS
+    # limits.
+    # (start version, end version)
+    ROOM_UPGRADE_AND_DOWNGRADE_PAIRINGS: list[tuple[str, str]]
+    # A list of upgrades or downgrades that should fall outside the limits defined by
+    # ALLOWED_ROOM_VERSIONS. These should all fail. Test starts with default room ver.
+    ROOM_UPGRADE_AND_DOWNGRADE_FAILURES: list[str]
 
     def prepare(self, reactor: MemoryReactor, clock: Clock, homeserver: HomeServer):
         super().prepare(reactor, clock, homeserver)
@@ -37,31 +89,6 @@ class RoomVersionCreateRoomTest(FederatingModuleApiTestCase):
 
         self.admin_b = self.register_user("b", "password", admin=True)
         self.access_token_b = self.login("b", "password")
-
-    def user_create_room(
-        self,
-        is_public: bool = False,
-        room_ver: str | None = None,
-        expect_code: int = HTTPStatus.OK,
-    ) -> str | None:
-        """
-        Helper to send an api request with a full set of required additional room state
-        to the room creation matrix endpoint. Returns a room_id if successful.
-
-        This differs from the create_local_room() helper, as it allows to designate
-        the room version and the expected response code. It also will subvert the
-        invitee_list, as it is not useful when determining if the room would be created.
-        """
-        # Hide the assertion from create_room_as() when the error code is unexpected. It
-        # makes errors for the tests less clear when all we get is the http response
-        return self.helper.create_room_as(
-            self.user_a,
-            is_public=is_public,
-            room_version=room_ver,
-            tok=self.access_token_a,
-            expect_code=expect_code,
-            extra_content=construct_extra_content(self.user_a, []),
-        )
 
     def upgrade_room_to_version(
         self,
@@ -97,94 +124,86 @@ class RoomVersionCreateRoomTest(FederatingModuleApiTestCase):
     def test_create_room_fails(self, _label: str, is_public: bool) -> None:
         """
         Test that most generic ways of not doing a room version string, and a room
-        version that is outside of what is wanted, fail
+        version that is outside what is allowed, fail
         """
-        self.user_create_room(
-            is_public=is_public,
-            room_ver="8",
-            expect_code=HTTPStatus.BAD_REQUEST,
-        )
-
-        self.user_create_room(
-            is_public=is_public,
-            room_ver=8,  # type: ignore[arg-type]
-            expect_code=HTTPStatus.BAD_REQUEST,
-        )
-
-        self.user_create_room(
-            is_public=is_public,
-            room_ver="11",
-            expect_code=HTTPStatus.BAD_REQUEST,
-        )
-
-        self.user_create_room(
-            is_public=is_public,
-            room_ver=11,  # type: ignore[arg-type]
-            expect_code=HTTPStatus.BAD_REQUEST,
-        )
-
-        self.user_create_room(
-            is_public=is_public,
-            room_ver="bad_version",
-            expect_code=HTTPStatus.BAD_REQUEST,
-        )
+        for room_version in self.ROOM_VERSIONS_THAT_SHOULD_FAIL:
+            room_id = self.create_local_room(
+                self.user_a,
+                is_public=is_public,
+                room_version=room_version,
+                expected_code=HTTPStatus.BAD_REQUEST,
+            )
+            assert room_id is None
 
     @parameterized.expand([("public", True), ("private", False)])
     def test_create_room_succeeds(self, _label: str, is_public: bool) -> None:
         """
-        Tests that a room version that is allowed succeeds
+        Tests that a room version that is allowed succeeds. This will use the default
+        room version since they are all itemized per the parameterize_class
         """
-        self.user_create_room(
+        room_id = self.create_local_room(
+            self.user_a,
             is_public=is_public,
-            room_ver="9",
-            expect_code=HTTPStatus.OK,
+            expected_code=HTTPStatus.OK,
         )
-        self.user_create_room(
-            is_public=is_public,
-            room_ver="10",
-            expect_code=HTTPStatus.OK,
-        )
+        assert room_id is not None
 
     @parameterized.expand([("public", True), ("private", False)])
-    def test_room_upgrades(self, _label: str, is_public: bool) -> None:
+    def test_room_upgrades_and_downgrades(self, _label: str, is_public: bool) -> None:
         """
-        Test room upgrades fail outside of defaults
+        Test room upgrades and downgrades work inside of limits, including "upgrading"
+        to the same room version
         """
-        # 9 -> 9 works
-        room_id = self.user_create_room(is_public=is_public, room_ver="9")
-        assert room_id
-        room_id = self.upgrade_room_to_version(room_id, "9", self.access_token_a)
-        assert room_id
+        for (
+            start_room_version,
+            finish_room_version,
+        ) in self.ROOM_UPGRADE_AND_DOWNGRADE_PAIRINGS:
 
-        # 10 -> 10 works
-        room_id = self.user_create_room(is_public=is_public, room_ver="10")
-        assert room_id
-        room_id = self.upgrade_room_to_version(room_id, "10", self.access_token_a)
-        assert room_id
+            # First test that a room can be "upgraded" into the same room version
+            room_id = self.create_local_room(
+                self.user_a, is_public=is_public, room_version=start_room_version
+            )
+            assert room_id is not None
+            room_id = self.upgrade_room_to_version(
+                room_id, start_room_version, self.access_token_a
+            )
+            assert room_id is not None
 
-        # 9 -> 10 works
-        room_id = self.user_create_room(is_public=is_public, room_ver="9")
-        assert room_id
-        room_id = self.upgrade_room_to_version(room_id, "10", self.access_token_a)
-        assert room_id
+            # Then test that a real version change works
+            room_id = self.create_local_room(
+                self.user_a, is_public=is_public, room_version=start_room_version
+            )
+            assert room_id is not None
+            room_id = self.upgrade_room_to_version(
+                room_id, finish_room_version, self.access_token_a
+            )
+            assert room_id is not None
 
-        # 9 -> 8 doesn't work
-        room_id = self.user_create_room(is_public=is_public, room_ver="9")
-        assert room_id
+    @parameterized.expand([("public", True), ("private", False)])
+    def test_room_version_fails_outside_of_allowed_limits(
+        self, _label: str, is_public: bool
+    ) -> None:
+        """
+        Test that changing a room version outside the limits fails, but works for an
+        admin. Each room starts with the DEFAULT_ROOM_VERSION
+        """
+        for change_room_version_to in self.ROOM_UPGRADE_AND_DOWNGRADE_FAILURES:
+            # First test that the "downgrade" fails with normal user
+            # This is a DEFAULT_ROOM_VERSION room
+            room_id = self.create_local_room(self.user_a, is_public=is_public)
+            assert room_id is not None
 
-        room_id = self.upgrade_room_to_version(room_id, "8", self.access_token_a)
-        assert room_id is None
+            room_id = self.upgrade_room_to_version(
+                room_id, change_room_version_to, self.access_token_a
+            )
+            # This is None because the room should fail
+            assert room_id is None
 
-        # 9 -> 8 requires an admin
-        room_id = self.helper.create_room_as(
-            self.admin_b,
-            is_public=is_public,
-            room_version="9",
-            tok=self.access_token_b,
-            expect_code=200,
-            extra_content=construct_extra_content(self.admin_b, []),
-        )
-        assert room_id
+            # Then make sure it works with an admin. This is a DEFAULT_ROOM_VERSION room
+            room_id = self.create_local_room(self.admin_b, is_public=is_public)
+            assert room_id is not None
 
-        room_id = self.upgrade_room_to_version(room_id, "8", self.access_token_b)
-        assert room_id
+            room_id = self.upgrade_room_to_version(
+                room_id, change_room_version_to, self.access_token_b
+            )
+            assert room_id is not None
